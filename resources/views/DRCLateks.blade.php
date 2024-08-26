d2b7068032ceb5e89d399f661fae8e9b00950fdc
kelvin verdian <85439200+kelvinverdian@users.noreply.github.com>
2024-08-26 11:51:27 +0700

    Data Kriteria

--- resources/views/DRCLateks.blade.php
+++ resources/views/DRCLateks.blade.php
@@ -4,26 +4,11 @@
 <div class="clearfix"></div>
 <div class="col-md-12 col-sm-12  ">
     <div class="x_panel">
-        <div class="x_title">
-            <h2>Table design <small>Custom design</small></h2>
-            <ul class="nav navbar-right panel_toolbox">
-                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
-                            class="fa fa-wrench"></i></a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">Settings 1</a>
-                        <a class="dropdown-item" href="#">Settings 2</a>
-                    </div>
-                </li>
-                <li><a class="close-link"><i class="fa fa-close"></i></a>
-                </li>
-            </ul>
-            <div class="clearfix"></div>
-        </div>
         <div class="x_content">
-            <p>Add class <code>bulk_action</code> to table for bulk actions options on row select</p>
+            <div class="d-flex justify-content-between">
+                <div><p class="h3">Data Kriteria</p></div>
+                <div><button class="btn btn-success"> <i class="bi bi-plus"></i> Tambah </button></div>
+            </div>
             <div class="table-responsive">
                 <table class="table table-striped jambo_table bulk_action">
                     <thead>
@@ -44,22 +29,15 @@
                     </thead>
                     <tbody>
                         <tr class="even pointer">
-                            <td class="a-center ">
-                            </td>
-                            <td class=" ">121000039</td>
-                            <td class=" ">May 28, 2014 11:30:12 PM</td>
-                            <td class=" ">121000208</td>
-                            <td class=" ">John Blank L</td>
+                            <td class="a-center"></td>
+                            <td class="align-middle">1</td>
+                            <td class="align-middle">121000039</td>
+                            <td class="align-middle">DRC</td>
+                            <td class="align-middle">50%</td>
                             <td class=" text-center">
-                                <button>
-                                    <a href="#">View</a>
-                                </button>
-                                <button>
-                                    <a href="#">Edit</a>
-                                </button>
-                                <button>
-                                    <a href="#">Delete</a>
-                                </button>
+                                <a href="#" class="btn btn-success btn-sm"><i class="bi bi-eye-fill"></i></a>
+                                <a href="#" class="btn btn-warning btn-sm"><i class="bi bi-pencil-square"></i></a>
+                                <a href="#" class="btn btn-danger btn-sm"><i class="bi bi-heartbreak"></i></a>
                             </td>
                         </tr>
                     </tbody>
--- resources/views/Layouts/main.blade.php
+++ resources/views/Layouts/main.blade.php
@@ -22,6 +22,9 @@
 
     <!-- Custom Theme Style -->
     <link href="{{ asset('gentelella/build/css/custom.min.css') }}" rel="stylesheet">
+
+    {{-- Custom Style --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body class="nav-md">
